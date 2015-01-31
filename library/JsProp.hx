c687e04931800579b81178314ac3efd65c6d1242
Yaroslav Sivakov <yar3333@gmail.com>
2015-01-31 04:45:43 +0300

    * support serialization & constructors with params

--- library/JsProp.hx
+++ library/JsProp.hx
@@ -1,6 +1,10 @@
 import haxe.macro.Context;
 import haxe.macro.Expr;
+import haxe.macro.Type;
+using haxe.macro.Tools;
 using Lambda;
+
+private typedef SuperClass = Null<{ t:Ref<ClassType>, params:Array<Type> }>;
 
 class JsProp
 {
@@ -10,15 +14,20 @@ class JsProp
 		{
 			var klass = Context.getLocalClass().get();
 			var fields = Context.getBuildFields();
-			var changed = false;
+			var codes = [];
 			for (field in fields)
 			{
 				if (hasMeta(field, ":property"))
 				{
-					if (processProperty(field, fields, klass.superClass != null)) changed = true;
+					var t = getDefinePropertyCode(field);
+					if (t != null) codes.push(t);
 				}
 			}
-			if (changed) return fields;
+			if (codes.length > 0)
+			{
+				addDeinePropertyCode(fields, klass.superClass, codes);
+				return fields;
+			}
 		}
 		return null;
 	}
@@ -29,22 +38,35 @@ class JsProp
 		{
 			var klass = Context.getLocalClass().get();
 			var fields = Context.getBuildFields();
-			var changed = false;
+			var codes = [];
 			for (field in fields)
 			{
 				switch (field.kind)
 				{
 					case FieldType.FProp(_, _, _, _):
-						if (processProperty(field, fields, klass.superClass != null)) changed = true;
+						var t = getDefinePropertyCode(field);
+						if (t != null) codes.push(t);
 					case _:
 				}
 			}
-			if (changed) return fields;
+			if (codes.length > 0)
+			{
+				addDeinePropertyCode(fields, klass.superClass, codes);
+				return fields;
+			}
 		}
 		return null;
 	}
 	
-	static function processProperty(field:Field, fields:Array<Field>, hasSuper:Bool) : Bool
+	static function addDeinePropertyCode(fields:Array<Field>, superClass:SuperClass, codes:Array<Expr>)
+	{
+		var code = macro $b{codes};
+		prependCode(getConstructorFunction(fields, superClass), code);
+		prependCode(getHxUnserializeFunction(fields, superClass), code);
+		ensureHxSerializeFunctionExists(fields, superClass);
+	}
+	
+	static function getDefinePropertyCode(field:Field) : Expr
 	{
 		switch (field.kind)
 		{
@@ -55,25 +77,19 @@ class JsProp
 				switch ([ get, set, hasMeta(field, ":isVar")])
 				{
 					case [ "get", "set", false ]:
-						var constructor = getConstructorFunction(fields, hasSuper);
-						var code = macro (untyped Object).defineProperty(this, $v{field.name}, { get:function() return $i{getter}(), set:function(v) $i{setter}(v) });
-						prependCode(constructor, code);
+						ensureNoExpr(e, field.pos);
 						field.kind = FieldType.FProp("default", "default", t, e);
-						return true;
+						return macro (untyped Object).defineProperty(this, $v{field.name}, { get:function() return $i{getter}(), set:function(v) $i{setter}(v) });
 						
 					case [ "get", "never", false ]:
-						var constructor = getConstructorFunction(fields, hasSuper);
-						var code = macro (untyped Object).defineProperty(this, $v{field.name}, { get:function() return $i{getter}() });
-						prependCode(constructor, code);
+						ensureNoExpr(e, field.pos);
 						field.kind = FieldType.FProp("default", "never", t, e);
-						return true;
+						return macro (untyped Object).defineProperty(this, $v{field.name}, { get:function() return $i{getter}() });
 						
 					case [ "never", "set", false ]:
-						var constructor = getConstructorFunction(fields, hasSuper);
-						var code = macro (untyped Object).defineProperty(this, $v{field.name}, { set:function(v) $i{setter}(v) });
-						prependCode(constructor, code);
+						ensureNoExpr(e, field.pos);
 						field.kind = FieldType.FProp("never", "default", t, e);
-						return true;
+						return macro (untyped Object).defineProperty(this, $v{field.name}, { set:function(v) $i{setter}(v) });
 						
 					case [ "default"|"null"|"never", "default"|"null"|"never", _ ]:
 						// nothing to do
@@ -85,7 +101,7 @@ class JsProp
 			case _:
 				Context.fatalError("JsProp: unsupported type (must be a property).", field.pos);
 		}
-		return false;
+		return null;
 	}
 	
 	static function prependCode(f:Function, code:Expr)
@@ -100,35 +116,82 @@ class JsProp
 		}
 	}
 	
-	
-	static function getConstructorFunction(fields:Array<Field>, hasSuper:Bool) : Function
+	static function getConstructorFunction(fields:Array<Field>, superClass:SuperClass) : Function
 	{
-		var constructor : Field = null;
+		var method : Field = null;
 		
-		for (field in fields)
+		for (field in fields) if (field.name == "new") { method = field; break; }
+		
+		if (method == null)
 		{
-			if (field.name == "new")
+			var superFuncArgs = getSuperFunctionArgs("new", superClass);
+			if (superFuncArgs != null)
 			{
-				constructor = field;
-				break;
+				var superCall = ECall(macro super, superFuncArgs.map(function(p) return macro $i{p.name}));
+				method = createMethod("new", superFuncArgs, macro:Void, { expr:superCall, pos:Context.currentPos() });
 			}
+			else
+			{
+				method = createMethod("new", [], macro:Void, macro {});
+			}
+			fields.push(method);
 		}
 		
-		if (constructor == null)
-		{
-			constructor = createMethod("new", [], macro : Void, hasSuper ? macro { super(); } : macro { } );
-			fields.push(constructor);
-		}
-		
-		switch (constructor.kind)
+		switch (method.kind)
 		{
 			case FieldType.FFun(f):
 				return f;
 				
 			case _:
-				Context.fatalError("JsProp: unexpected constructor type '" + constructor.kind + "'.", constructor.pos);
+				Context.fatalError("JsProp: unexpected constructor type '" + method.kind + "'.", method.pos);
 				return null;
 		}
+	}
+	
+	static function getHxUnserializeFunction(fields:Array<Field>, superClass:SuperClass) : Function
+	{
+		var method : Field = null;
+		
+		for (field in fields) if (field.name == "hxUnserialize") { method = field; break; }
+		
+		if (method == null)
+		{
+			var superFuncArgs = getSuperFunctionArgs("hxUnserialize", superClass);
+			if (superFuncArgs != null)
+			{
+				var superCall = ECall(macro super.hxUnserialize, superFuncArgs.map(function(p) return macro $i{p.name}));
+				method = createMethod("hxUnserialize", superFuncArgs, macro:Void, { expr:superCall, pos:Context.currentPos() } );
+				method.access.push(Access.AOverride);
+			}
+			else
+			{
+				method = createMethod("hxUnserialize", [ { name:"s", type:(macro:haxe.Unserializer) } ], macro:Void, macro { s.unserializeObject(cast this); } );
+				if (method.meta == null) method.meta = [];
+				method.meta.push({ name:":access", params:[ macro haxe.Unserializer.unserializeObject ], pos:Context.currentPos() });
+			}
+			fields.push(method);
+		}
+		
+		switch (method.kind)
+		{
+			case FieldType.FFun(f):
+				return f;
+				
+			case _:
+				Context.fatalError("JsProp: unexpected hxUnserialize method type '" + method.kind + "'.", method.pos);
+				return null;
+		}
+	}
+	
+	static function ensureHxSerializeFunctionExists(fields:Array<Field>, superClass:SuperClass) : Void
+	{
+		for (field in fields) if (field.name == "hxSerialize") return;
+		if (getSuperClassField("hxSerialize", superClass) != null) return;
+		
+		var method = createMethod("hxSerialize", [ { name:"s", type:(macro:haxe.Serializer) } ], macro:Void, macro { s.serializeFields(cast this); } );
+		if (method.meta == null) method.meta = [];
+		method.meta.push({ name:":access", params:[ macro haxe.Serializer.serializeFields ], pos:Context.currentPos() });
+		fields.push(method);
 	}
 	
 	static function hasMeta(f:{ meta:Metadata }, m:String) : Bool
@@ -156,5 +219,53 @@ class JsProp
 						})
 			, pos : expr.pos
 		};
+	}
+	
+	
+	
+	static function getSuperClassField(name:String, superClass:SuperClass) : ClassField
+	{
+		while (superClass != null)
+		{
+			var c = superClass.t.get();
+			if (name == "new" && c.constructor != null) return c.constructor.get();
+			var fields = c.fields.get().filter(function(f) return f.name == name);
+			if (fields.length > 0) return fields[0];
+			superClass = c.superClass;
+		}
+		return null;
+	}
+	
+	static function getSuperFunctionArgs(name:String, superClass:SuperClass) : Array<FunctionArg>
+	{
+		var field = getSuperClassField(name, superClass);
+		if (field != null)
+		{
+			switch (field.type)
+			{
+				case Type.TFun(args, ret): return args.map(toFunctionArg);
+				case Type.TLazy(f):
+					switch (f())
+					{
+						case Type.TFun(args, ret): return args.map(toFunctionArg);
+						case _: Context.fatalError("Expected TFun: " + field.type, field.pos);
+					}
+				case _: Context.fatalError("Expected TFun: " + field.type, field.pos);
+			}
+		}
+		return null;
+	}
+	
+	static function toFunctionArg(a:{ name:String, opt:Bool, t:Type }) : FunctionArg
+	{
+		return { name: a.name, opt: a.opt, type: a.t.toComplexType(), value: null };
+	}
+	
+	static function ensureNoExpr(e:Null<Expr>, pos:Position)
+	{
+		if (e != null)
+		{
+			Context.fatalError("Default value is not supported here.", pos);
+		}
 	}
 }
--- tests/src/Main.hx
+++ tests/src/Main.hx
@@ -1,7 +1,33 @@
-@:build(JsProp.all())
-class TestClass
+import haxe.Serializer;
+import haxe.Unserializer;
+
+class BaseClass
 {
-    public var r = 5;
+    var r : Int;
+	
+	public function new(p:Int)
+	{
+		r = p;
+		trace("MYVALUE "+ p);
+	}
+	
+	function hxSerialize(s:Serializer)
+	{
+		trace("BaseClass.hxSerialize");
+		s.serialize(r);
+	}
+	
+	function hxUnserialize(s:Unserializer)
+	{
+		trace("BaseClass.hxUnserialize");
+		r = s.unserialize();
+	}
+}
+
+
+@:build(JsProp.all())
+class TestClass extends BaseClass
+{
 	
     public var myPropGetSet(get, set) : Int;
     function get_myPropGetSet() return r;
@@ -9,9 +35,13 @@ class TestClass
     
 	public var myPropGetNever(get, never) : Int;
     function get_myPropGetNever() return r;
+	
+	override function hxSerialize(s:Serializer) super.hxSerialize(s);
+	override function hxUnserialize(s:Unserializer) super.hxUnserialize(s);
     
 	public function new()
 	{
+		super(5);
 		trace("TestClass.new: raw this.myPropGetSet = " + untyped __js__("this.myPropGetSet"));
 		trace("TestClass.new: haxe myPropGetSet = " + myPropGetSet);
 	}
@@ -19,7 +49,6 @@ class TestClass
 
 typedef TestTypedef =
 {
-    var r : Int;
 	public var myPropGetSet(default, default) : Int;
     public var myPropGetNever(default, never) : Int;
 }
@@ -28,10 +57,20 @@ class Main
 {
 	static function main()
 	{
-		var klass : TestTypedef = new TestClass();
+		test(new TestClass());
 		
-		trace("Out must be " + klass.r);
+		trace("");
+		trace("Serializing test:");
 		
+		var klass = new TestClass();
+		var s = Serializer.run(klass);
+		trace("serialized = " + s);
+		klass = Unserializer.run(s);
+		test(klass);
+	}
+	
+	static function test(klass:TestTypedef)
+	{
 		trace("raw klass.myPropGetSet = " + untyped __js__("klass.myPropGetSet"));
 		trace("raw klass.myPropGetNever = " + untyped __js__("klass.myPropGetNever"));
 		
